ecd055045dc4b7dff1efe21fbe92cfcd12920c35
Atul Gaikar <84188234+Atul-Gaikar@users.noreply.github.com>
2021-11-11 16:13:10 +0530

    [Atul]Add: added Replace String Templet

---
+++ ReplaceStringTemplet.py
@@ -0,0 +1,27 @@
+"""
+   * Author - Atul Gaikar
+   * Date -  07-Nov-2021
+   * Time -  3:15 PM
+   * Title - User Input and Replace String Template
+"""
+flag = True
+while flag == True:
+    try:
+
+        """
+           Description:
+           :we are replace the string Template "Hello <<UserName>>, How are you?"
+           parameters:
+           :in we are assign input String format"""
+        name = input("Please Enter Your Name:")
+        name = str(name)
+        nameLength = len(name)
+        if nameLength >= 3:
+            print(nameLength)
+            print("Hello " + name + ", How are You ?")
+            flag = False
+            raise NameError
+        else:
+            print("Invalid Name")
+    except NameError:
+        print("Enter Correct Name")
